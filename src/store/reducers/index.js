455376ecebdd53e135682220b1f538c0b9566c33
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-05-19 17:29:27 +0200

    Fixes

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -4,12 +4,14 @@ import { combineReducers } from 'redux';
 // project import
 import menu from './menu';
 import snackbar from './snackbar';
+import calendar from './calendar';
 import personalInformation from './personalInformation';
 // ==============================|| COMBINE REDUCERS ||============================== //
 
 const reducers = combineReducers({
   menu,
   snackbar,
+  calendar,
   personalInformation
 });
 
