f056c4852fbe2042033ac74e6524ba3cda8fb613
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-08-04 10:53:13 +0200

    Fixes

--- src/components/InfoWrapper.js
+++ src/components/InfoWrapper.js
@@ -1,9 +1,11 @@
-import { Stack, Tooltip } from '@mui/material';
+import { Stack, Tooltip, ClickAwayListener } from '@mui/material';
+import { useState } from 'react';
 import { InfoCircleTwoTone } from '@ant-design/icons';
 import { useIntl } from 'react-intl';
 import { useTheme } from '@mui/material/styles';
 
 const InfoWrapper = ({ children, tooltipText }) => {
+  const [open, setOpen] = useState(false);
   const theme = useTheme();
   const intl = useIntl();
 
@@ -11,9 +13,24 @@ const InfoWrapper = ({ children, tooltipText }) => {
     (
       <Stack direction="row" alignItems="center" spacing={0.8}>
         {children}
-        <Tooltip title={intl.formatMessage({ id: tooltipText })} placement="top">
-          <InfoCircleTwoTone twoToneColor={theme.palette.primary.main} />
-        </Tooltip>
+
+        <ClickAwayListener onClickAway={() => { setOpen(false); }}>
+          <Tooltip
+            PopperProps={{
+              disablePortal: true,
+            }}
+            onClose={() => { setOpen(false); }}
+            open={open}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            title={intl.formatMessage({ id: tooltipText })}
+            placement="top"
+          >
+            <InfoCircleTwoTone twoToneColor={theme.palette.primary.main} onClick={() => { setOpen(true); }} />
+          </Tooltip>
+        </ClickAwayListener>
+
       </Stack>
     ) :
     (
