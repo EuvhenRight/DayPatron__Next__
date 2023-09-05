a52281ff88b9bc08db0ce8c46950c4c040e1060c
ASC10101 <139119005+ASC10101@users.noreply.github.com>
2023-09-05 16:16:51 +0200

    ASUpdate support-page.js
    test duplicating

--- src/pages/support-page.js
+++ src/pages/support-page.js
@@ -45,5 +45,39 @@ const SupportPage = () => {
     </Grid >
   );
 };
+ return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <WelcomeBanner title="10x Support Hub" subTitle=" Welcome to the 10x Support Hub - your pathway to success made simple." />
+      </Grid>
+      <Grid item xs={12}>
+        <MainCard>
+          We&apos;re here to enhance your journey with helpful support videos that cover everything you need to know. From start to finish, we&apos;ve got you covered.
+          And if you ever need a hand, just click the &ldquo;Need Help&ldquo; button on the bottom left. It&apos;s your direct line to our experts, available in Chrome or with the Loom desktop app. Your journey matters, and we&apos;re here to make it a smooth and successful one.
+        </MainCard>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={4}>
+
+            <MainCard title="How to contact us for Support">
+              <Stack spacing={2}>
+                <Typography color="secondary">
+                  This is a short description about the video
+                </Typography>
+                <CardMedia
+                  sx={{ minHeight: 300 }}
+                  component="iframe"
+                  src="https://www.loom.com/share/c272576ad1ce4c6286d6dcf80ecdbc68"
+                  allowFullScreen={true} />
+              </Stack>
+            </MainCard>
+
+          </Grid>
+        </Grid>
+      </Grid >
+    </Grid >
+  );
+};
 
 export default SupportPage;
