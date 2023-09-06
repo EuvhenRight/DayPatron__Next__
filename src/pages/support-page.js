e2b38d00dd15fd53c75ab28d77edef937f6d5f27
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-09-06 13:21:54 +0200

    Fixes

--- src/pages/support-page.js
+++ src/pages/support-page.js
@@ -1,6 +1,5 @@
 import MainCard from 'components/MainCard';
 import WelcomeBanner from 'sections/WelcomeBanner';
-import SanitizedHTML from 'react-sanitized-html';
 
 // material-ui
 import {
@@ -11,57 +10,6 @@ import {
 } from '@mui/material';
 
 const SupportPage = () => {
-  const guiddeHtml = `<div>
-<div style="text-align:center"><div><h2><a href="https://app.guidde.com/playbooks/qUVgPEuJstPTsxUxLNuYc9" target="_blank" rel="noreferrer">How to adjust your email address</a></h2></div></div>
-
-<div style="position:relative;padding-bottom:56.25%;">
-<iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px" 
-      src="https://embed.app.guidde.com/playbooks/qUVgPEuJstPTsxUxLNuYc9"
-      title="How to adjust your email address"
-      frameborder="0"
-      allowfullscreen="true"
-      allow="clipboard-write"
-      sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-forms allow-same-origin allow-presentation"></iframe>
-</div>
-
-<div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Learn how to adjust your email address in just a few simple steps. Follow this guide to update your email address and ensure that your contact information is up to date.</p>
-
-</div> <h3 style="width: 100%; word-break: break-word; max-width:100%;">
-                      Go to <a href="https://talent-portal.10x.team" target="_blank">talent-portal.10x.team</a>
-</h3> <h3 style="width: 100%; word-break: break-word; max-width:100%;">1. Click "Personal Information"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Access your personal information settings.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2F6n86Fk5sqSoQ8hRy8xXPU6_doc.png?alt=media&amp;token=6971e3df-016d-4749-989d-3697c217dd5f" alt="Click 'Personal Information'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">2. Click "angelique@10x.team"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Select your current email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2F9ZHQZ4sccbzx9SLHUq6vgQ_doc.png?alt=media&amp;token=95351577-2ba2-4330-951e-b2e044e7b181" alt="Click 'angelique@10x.team'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">3. Fill "angelique@trailblazings.com"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Enter your new email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2Fk1jJ31Xpbq4ZHAuGgsdSVq_doc.png?alt=media&amp;token=edbb996f-f073-4dce-bd03-fb432dd98687" alt="Fill 'angelique@trailblazings.com'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">4. Click "Home/Personal InformationPersonal InformationUploadAngelique Schoutenangelique@10x.teamFirst NameLast NameEmail AddressPhone NumberLinkedIn PagePera Interview UrlCalendly..."</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Save your changes to update your email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2FwHVX54fdFBc4onthyTj5fy_doc.png?alt=media&amp;token=a4e6e12e-a53e-46b3-b435-f6912808904c" alt="Click 'Home/Personal InformationPersonal InformationUploadAngelique Schoutenangelique@10x.teamFirst NameLast NameEmail AddressPhone NumberLinkedIn PagePera Interview UrlCalendly...'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">5. Click "Save"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Confirm your new email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2FdZQZoXmPYwGykXXFNdMAoW_doc.png?alt=media&amp;token=168c96f5-c699-436e-8b47-2474803dbeb5" alt="Click 'Save'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">6. Click "Save"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Finalize the change to your email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2Fuh8zurXGoi9cZ6B1cdwk4v_doc.png?alt=media&amp;token=52c2f9fd-6a22-482f-aaa9-3adb1b3a86b2" alt="Click 'Save'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">7. Click "Save"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Complete the process of adjusting your email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2F3pqFbpMctvdCZeicrjf1fs_doc.png?alt=media&amp;token=fc997eba-13aa-4e53-b180-a2346406ebde" alt="Click 'Save'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">8. Click "angelique@trailblazings.com"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Select your current email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2FbZpN1ibNMLMS7BLPNgE7ve_doc.png?alt=media&amp;token=68441651-f588-4dcc-8072-28bdf5f03f10" alt="Click 'angelique@trailblazings.com'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">9. Fill "angelique@10x.team"</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Enter your new email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2FgUZUfaDrkJWzkY3Ufhdzc2_doc.png?alt=media&amp;token=675fd57b-51c1-488c-8073-d0b52ca6228a" alt="Fill 'angelique@10x.team'" /> <h3 style="width: 100%; word-break: break-word; max-width:100%;">10. Click "Home/Personal InformationPersonal InformationUploadAngelique Schoutenangelique@10x.teamFirst NameLast NameEmail AddressPhone NumberLinkedIn PagePera Interview UrlCalendly..."</h3> <div style="margin-top:16px; margin-bottom:16px" >
-<p style="font-size:14px; color: rgba(9, 12, 16, 0.6); width: 100%; word-break: break-word; max-width:100%;">Save your changes to update your email address.</p>
-
-</div> <img width="100%" src="https://static.guidde.com/v0/qg%2FSxvqoy3UEWei5eIT9aEjwkOw8FB2%2FqUVgPEuJstPTsxUxLNuYc9%2FsNeDWDyiLDJt5a8x4bHvqY_doc.png?alt=media&amp;token=317cc566-ddb5-4f4a-a232-3d16eb3f804c" alt="Click 'Home/Personal InformationPersonal InformationUploadAngelique Schoutenangelique@10x.teamFirst NameLast NameEmail AddressPhone NumberLinkedIn PagePera Interview UrlCalendly...'" />
-<div style="margin-right:2px"><a href="https://www.guidde.com/" rel="noreferrer" target="_blank" style="text-decoration:none;color:#000000">Powered by <strong style="color:#CB0000">Guidde</strong></a></div>
-</div>`;
 
   return (
     <Grid container spacing={3}>
@@ -84,7 +32,7 @@ const SupportPage = () => {
                   This is a short description about the video
                 </Typography>
                 <CardMedia
-                  sx={{ minHeight: 300 }}
+                  sx={{ minHeight: 300, border: 0 }}
                   component="iframe"
                   src="https://www.loom.com/embed/c272576ad1ce4c6286d6dcf80ecdbc68?sid=71623775-5405-4f09-970e-b798e1b29617%22"
                   allowFullScreen={true} />
@@ -92,14 +40,6 @@ const SupportPage = () => {
             </MainCard>
           </Grid>
           {/* ---------------- END COPY ---------------- */}
-
-          <Grid item xs={12}>
-            <MainCard>
-
-              <SanitizedHTML html={guiddeHtml} />
-
-            </MainCard>
-          </Grid>
 
         </Grid>
       </Grid >
