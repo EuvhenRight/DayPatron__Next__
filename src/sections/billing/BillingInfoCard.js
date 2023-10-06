90912c8ddbeac9b4010e38518c99f773cf1ad767
cwlam <chiwing.lam@gmail.com>
2023-10-06 12:58:06 +0200

    Changed date time format in BillingInfoCard

--- src/sections/billing/BillingInfoCard.js
+++ src/sections/billing/BillingInfoCard.js
@@ -97,7 +97,7 @@ const BillingInfoCard = ({ billingInfo }) => {
         </Grid>
         <Grid item xs={8}>
           <Typography variant="body1" color="secondary">
-            {format(new Date(billingInfo.createdAtUtc), "yyyy-MM-dd")}
+            {format(new Date(billingInfo.createdAtUtc), "yyyy-MM-dd HH:mm:ss")}
           </Typography>
         </Grid>
         <Grid item xs={12}>
