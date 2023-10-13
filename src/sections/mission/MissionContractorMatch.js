534ecdfe08f3d8f502133c1f8fb95b1a07e977c0
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-10-13 10:23:17 +0200

    Hide missing strength/growthOpp

--- src/sections/mission/MissionContractorMatch.js
+++ src/sections/mission/MissionContractorMatch.js
@@ -387,6 +387,9 @@ const MissionContractorMatch = ({ missionId, contractorId }) => {
   if (tabGroupId === peraResponseResultTabGroup)
     selectedPeraAssessment = missionContractorMatch?.contractorPeraSurveyResponse?.responseResultsTree[tabGroupItemIndex];
 
+  let strengthTraitResults = selectedPeraAssessment?.traitResults?.filter(x => x.isStrength);
+  let growthOpportunityTraitResults = selectedPeraAssessment?.traitResults?.filter(x => x.isGrowthOpportunity);
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} sm={5} md={4} xl={3}>
@@ -619,40 +622,47 @@ const MissionContractorMatch = ({ missionId, contractorId }) => {
                   </Grid>
                   <Grid item xs={12}>
                     <Grid container spacing={2}>
-                      <Grid item xs={12} md={6}>
-                        <Grid container spacing={1}>
-                          <Grid item xs={12}>
-                            <Chip label="STRENGTH" size="small" color="success" className="small-chip" />
+
+                      {strengthTraitResults?.length > 0 &&
+                        <Grid item xs={12} md={6}>
+                          <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                              <Chip label="STRENGTH" size="small" color="success" className="small-chip" />
+                            </Grid>
+                            {strengthTraitResults?.map((traitResult, traitResultIndex) => {
+                              return (
+                                <Grid key={traitResultIndex} item xs={12}>
+                                  <ButtonBase component={Link} onClick={() => { handleOpenTraitDetails(traitResult); }} underline="none">
+                                    <Typography variant="h5" sx={{ mr: 1.25 }} >{traitResult?.trait?.hrPage?.title}</Typography>
+                                    <RightOutlined />
+                                  </ButtonBase>
+                                </Grid>
+                              );
+                            })}
                           </Grid>
-                          {selectedPeraAssessment?.traitResults?.filter(x => x.isStrength)?.map((traitResult, traitResultIndex) => {
-                            return (
-                              <Grid key={traitResultIndex} item xs={12}>
-                                <ButtonBase component={Link} onClick={() => { handleOpenTraitDetails(traitResult); }} underline="none">
-                                  <Typography variant="h5" sx={{ mr: 1.25 }} >{traitResult?.trait?.hrPage?.title}</Typography>
-                                  <RightOutlined />
-                                </ButtonBase>
-                              </Grid>
-                            );
-                          })}
                         </Grid>
-                      </Grid>
-                      <Grid item xs={12} md={6}>
-                        <Grid container spacing={1}>
-                          <Grid item xs={12}>
-                            <Chip label="GROWTH OPPORTUNITY" size="small" color="error" className="small-chip" />
+                      }
+
+                      {growthOpportunityTraitResults?.length > 0 &&
+                        <Grid item xs={12} md={6}>
+                          <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                              <Chip label="GROWTH OPPORTUNITY" size="small" color="error" className="small-chip" />
+                            </Grid>
+                            {growthOpportunityTraitResults?.map((traitResult, traitResultIndex) => {
+                              return (
+                                <Grid key={traitResultIndex} item xs={12}>
+                                  <ButtonBase component={Link} onClick={() => { handleOpenTraitDetails(traitResult); }} underline="none">
+                                    <Typography variant="h5" sx={{ mr: 1.25 }} >{traitResult?.trait?.hrPage?.title}</Typography>
+                                    <RightOutlined />
+                                  </ButtonBase>
+                                </Grid>
+                              );
+                            })}
                           </Grid>
-                          {selectedPeraAssessment?.traitResults?.filter(x => x.isGrowthOpportunity)?.map((traitResult, traitResultIndex) => {
-                            return (
-                              <Grid key={traitResultIndex} item xs={12}>
-                                <ButtonBase component={Link} onClick={() => { handleOpenTraitDetails(traitResult); }} underline="none">
-                                  <Typography variant="h5" sx={{ mr: 1.25 }} >{traitResult?.trait?.hrPage?.title}</Typography>
-                                  <RightOutlined />
-                                </ButtonBase>
-                              </Grid>
-                            );
-                          })}
                         </Grid>
-                      </Grid>
+                      }
+                      
                     </Grid>
                   </Grid>
                   <Grid item xs={12}>
