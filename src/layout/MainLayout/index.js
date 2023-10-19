919a867072f636dde1fe345760b6f91d7c4527e7
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-10-19 09:56:50 +0200

    Use current user for intercom

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -29,6 +29,7 @@ const MainLayout = () => {
   const dispatch = useDispatch();
 
   const menu = useSelector((state) => state.menu);
+  const personalInformation = useSelector((state) => state.personalInformation);
   const { drawerOpen } = menu;
 
   const isHorizontal = menuOrientation === LAYOUT_CONST.HORIZONTAL_LAYOUT && !downLG;
@@ -53,6 +54,28 @@ const MainLayout = () => {
     if (open !== drawerOpen) setOpen(drawerOpen);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [drawerOpen]);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.text =
+`window.intercomSettings = {
+  api_base: 'https://api-iam.intercom.io',
+  app_id: 'nohezlna',
+  name: '` + personalInformation?.firstName + ' ' + personalInformation?.lastName + `', // Full name
+  email: '` + personalInformation?.email + `', // Email address
+  created_at: '` + Math.floor(new Date(personalInformation?.createdAtUtc).getTime() / 1000) + `' // Signup date as a Unix timestamp
+};
+
+(function () { var w = window; var ic = w.Intercom; if (typeof ic === "function") { ic('reattach_activator'); ic('update', w.intercomSettings); } else { var d = document; var i = function () { i.c(arguments); }; i.q = []; i.c = function (args) { i.q.push(args); }; w.Intercom = i; var l = function () { var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://widget.intercom.io/widget/nohezlna'; var x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); }; if (document.readyState === 'complete') { l(); } else if (w.attachEvent) { w.attachEvent('onload', l); } else { w.addEventListener('load', l, false); } } })();
+`;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, [personalInformation]);
 
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
