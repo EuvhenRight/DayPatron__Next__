ae88ccbb2f99ce961bf2bc4d53eaefcbce989cdb
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-10-29 00:58:06 +0200

    Added api loader

--- src/store/reducers/actions.js
+++ src/store/reducers/actions.js
@@ -2,3 +2,4 @@ export const LOGIN = '@auth/LOGIN';
 export const LOGOUT = '@auth/LOGOUT';
 export const PERSONAL_INFORMATION_UPDATE = '@personalInformation/UPDATE';
 export const PERSONAL_INFORMATION_GET = '@personalInformation/GET';
+export const LOADING_DETAILS_UPDATE = '@loadingDetails/UPDATE';
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -6,13 +6,15 @@ import menu from './menu';
 import snackbar from './snackbar';
 import personalInformation from './personalInformation';
 import invoice from './invoice';
+import loadingDetails from './loadingDetails';
 // ==============================|| COMBINE REDUCERS ||============================== //
 
 const reducers = combineReducers({
   menu,
   snackbar,
   personalInformation,
-  invoice
+  invoice,
+  loadingDetails
 });
 
 export default reducers;
