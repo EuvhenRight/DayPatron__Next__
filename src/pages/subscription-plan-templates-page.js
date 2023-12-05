e423476e37dd0fcdbb83f46bda0e3fd5ee9a17e2
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2023-12-05 12:09:39 +0100

    Fixes

--- src/pages/subscription-plan-templates-page.js
+++ src/pages/subscription-plan-templates-page.js
@@ -2,15 +2,20 @@ import { useState, useEffect } from 'react';
 import { 
   Grid,
   Button,
-  Stack
+  Stack,
+  Typography
 } from '@mui/material';
 import { useKeycloak } from '@react-keycloak/web';
 import MainCard from 'components/MainCard';
 import SubscriptionPlanTemplateCard from 'sections/subscription/SubscriptionPlanTemplateCard';
+import { prepareApiBody } from 'utils/stringUtils';
+import { useDispatch } from 'react-redux';
+import { openSnackbar } from 'store/reducers/snackbar';
 
 const SubscriptionPlanTemplatesPage = () => {
   const { keycloak } = useKeycloak();
   const [templates, setTemplates] = useState([]);
+  const dispatch = useDispatch();
   
   useEffect(() => {
     (async () => {
@@ -54,10 +59,74 @@ const SubscriptionPlanTemplatesPage = () => {
   }
 
   const handleAddTemplate = () => {
-    let newTemplates = [...templates, {}];
+    let newTemplates = [...templates, {rateType: 'Monthly'}];
 
     setTemplates(newTemplates);
   }
+  
+  const handleSaveClick = async () => {
+
+    try {
+      let response = await fetch(process.env.REACT_APP_JOBMARKET_API_BASE_URL + '/subscription-plans/templates',
+        {
+          method: 'PUT',
+          headers: {
+            'Authorization': 'Bearer ' + keycloak.idToken,
+            'Content-Type': 'application/json'
+          },
+          body: prepareApiBody({templates})
+        }
+      );
+
+      if (!response.ok) {
+        dispatch(
+          openSnackbar({
+            open: true,
+            message: 'Update failed.',
+            variant: 'alert',
+            alert: {
+              color: 'error'
+            },
+            close: false
+          })
+        );
+
+        return;
+      }
+
+      let json = await response.json();
+      setTemplates(json?.templates);
+
+      dispatch(
+        openSnackbar({
+          open: true,
+          message: 'Updated.',
+          variant: 'alert',
+          alert: {
+            color: 'success'
+          },
+          close: false
+        })
+      );
+
+    } catch (err) {
+      dispatch(
+        openSnackbar({
+          open: true,
+          message: 'Update failed.',
+          variant: 'alert',
+          alert: {
+            color: 'error'
+          },
+          close: false
+        })
+      );
+      console.log(err);
+    }
+  };
+
+  if (!keycloak.tokenParsed.roles.includes('admin'))
+  return <Typography>Unauthorized</Typography>
 
   return (
     <Grid container spacing={1}>
@@ -77,11 +146,18 @@ const SubscriptionPlanTemplatesPage = () => {
       <Grid item xs={12} md={4}>
         <MainCard>
           <Stack justifyContent="center" alignItems="center" sx={{height: 312}}>
-            <Button onClick={handleAddTemplate} color="primary" variant="contained">
+            <Button onClick={handleAddTemplate} variant="outlined">
               Add
             </Button>
           </Stack>
         </MainCard>
+      </Grid>
+      <Grid item xs={12}>
+        <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={2} sx={{ mt: 2.5 }}>
+          <Button onClick={handleSaveClick} color="primary" variant="contained">
+            Save
+          </Button>
+        </Stack>
       </Grid>
     </Grid>
   );
