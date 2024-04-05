91b09aa3ecdcdf5d71e760de5441c2bfe87bb5d1
EuvhenRight <ugnivenko.ea@gmail.com>
2024-04-05 23:00:53 +0200

    add cartItem in cart

--- app/api/cart/[userId]/add-item/route.ts
+++ app/api/cart/[userId]/add-item/route.ts
@@ -9,10 +9,7 @@ export async function POST(
 ) {
 	try {
 		// Parse the request body as JSON
-		const body = await request.text()
-		const requestBody = JSON.parse(body)
-
-		const product: Product = requestBody
+		const product: Product = await request.json()
 
 		// Validation check for productId and quantity
 		if (!product) {
@@ -22,44 +19,82 @@ export async function POST(
 			)
 		}
 
+		// Find the cart for the user
 		const cart = await prisma.cart.findUnique({
-			where: { itemsTotal: 0 },
-			include: { items: { include: { product: true } } },
+			where: { userId },
+			include: { items: true },
 		})
 
+		// Calculate total price and total items
+		const totalPrice =
+			cart?.items.reduce((acc, item) => {
+				const price =
+					item.discount_price !== 0 ? item.discount_price : item.original_price
+				return acc + price * item.quantity
+			}, 0) || 0
+
+		const totalItems =
+			cart?.items.reduce((acc, item) => acc + item.quantity, 0) || 0
+
+		// Create or update the cart
 		const newCart =
 			cart ??
 			(await prisma.cart.create({
-				data: { userId, items: { create: [] }, subTotal: 0, itemsTotal: 0 },
+				data: {
+					userId,
+					items: { create: [] },
+					subTotal: totalPrice,
+					itemsTotal: totalItems,
+				},
 				include: { items: { include: { product: true } } },
 			}))
-		console.log(newCart, 'newCart')
-		console.log(cart, 'cart')
+
+		// Update cart totals if the cart already existed
+		if (cart) {
+			await prisma.cart.update({
+				where: { userId },
+				data: {
+					subTotal: totalPrice,
+					itemsTotal: totalItems,
+				},
+			})
+		}
+
+		// Find existing cart item
 		const existingCartItem = cart?.items.find(
-			item => item.product.id === product.id
+			item => item.productId === product.id
 		)
 
 		if (existingCartItem) {
 			// If the product exists, update the quantity
 			const updatedCartItem = await prisma.cartItem.update({
 				where: { id: existingCartItem.id },
-				data: {
-					quantity: {
-						increment: 1,
-					},
-				},
+				data: { quantity: { increment: 1 } },
 			})
-			return NextResponse.json({ CartItem: updatedCartItem }, { status: 200 })
+
+			return NextResponse.json(
+				{ cart: newCart, cartItem: updatedCartItem },
+				{ status: 200 }
+			)
 		} else {
 			// If the product doesn't exist, create a new cart item
 			const newCartItem = await prisma.cartItem.create({
 				data: {
+					name: product.name,
+					article: product.article,
+					volume: product.volume,
+					original_price: product.original_price,
+					discount_price: product.discount_price,
 					productId: product.id,
 					quantity: 1,
 					cartId: newCart.id,
 				},
 			})
-			return NextResponse.json({ cartItem: newCartItem }, { status: 200 })
+
+			return NextResponse.json(
+				{ cart: newCart, cartItem: newCartItem },
+				{ status: 200 }
+			)
 		}
 	} catch (error: any) {
 		console.error('Error adding item to cart:', error)
