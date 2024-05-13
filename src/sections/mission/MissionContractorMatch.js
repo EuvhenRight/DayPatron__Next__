703a936cbf6f5210632b87fa3830ee79981870bf
Anton Zhelyazkov <azhelyazkov@hotmail.nl>
2024-05-13 17:25:13 +0200

    Fixes

--- src/sections/mission/MissionContractorMatch.js
+++ src/sections/mission/MissionContractorMatch.js
@@ -816,149 +816,146 @@ const MissionContractorMatch = ({ missionId, contractorId }) => {
               </Grid>
             }
             {tabGroupId === professionalExperiencesTabGroup &&
-              <Grid container spacing={3}>
-                
-                <Grid item xs={12}>
-                  <MainCard>
-                    <Grid container spacing={3}>
-                      <Grid item xs={12}>
-                        <Typography variant="h3">Experience</Typography>
-                      </Grid>
-                      {missionContractorMatch?.contractor?.professionalExperiences?.map((professionalExperience, professionalExperienceIndex) => {
-                        return (<Grid key={professionalExperienceIndex} item xs={12}>
-                          <Grid container spacing={2}>
-                            <Grid item>
-                              {professionalExperience?.companyLogoUrl ? 
-                              (
-                                <img
-                                  style={{ width: 50, height: 50, textDecoration: 'none', opacity: 1 }}
-                                  alt={professionalExperience?.company}
-                                  src={professionalExperience?.companyLogoUrl}
-                                />
-                              ) : 
-                              (
-                                <ShopOutlined style={{ fontSize: '3rem' }}  />
-                              )}
-                            </Grid>
-                            <Grid item xs zeroMinWidth>
-                              <Typography align="left" variant="h5">
-                                {professionalExperience?.title}
-                              </Typography>
-                              <Typography variant="h6">
-                                {professionalExperience?.company}
-                              </Typography>
-                              <Typography color="secondary">
-                                {professionalExperience?.location}
-                              </Typography>
-                              <Typography color="secondary">
-                                {format(new Date(professionalExperience?.startDateUtc), 'MMM y')}&nbsp;-&nbsp;{professionalExperience?.endDateUtc ? format(new Date(professionalExperience?.endDateUtc), 'MMM y') : 'Present'}
-                              </Typography>
-                              <Typography align="left" variant="body1">
-                                {professionalExperience?.description}
-                              </Typography>
-                            </Grid>
+              <MainCard>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Typography variant="h3">Experience</Typography>
+                  </Grid>
+                  {missionContractorMatch?.contractor?.professionalExperiences?.length > 0 ?
+                    missionContractorMatch?.contractor?.professionalExperiences?.map((professionalExperience, professionalExperienceIndex) => {
+                      return (<Grid key={professionalExperienceIndex} item xs={12}>
+                        <Grid container spacing={2}>
+                          <Grid item>
+                            {professionalExperience?.companyLogoUrl ? 
+                            (
+                              <img
+                                style={{ width: 50, height: 50, textDecoration: 'none', opacity: 1 }}
+                                alt={professionalExperience?.company}
+                                src={professionalExperience?.companyLogoUrl}
+                              />
+                            ) : 
+                            (
+                              <ShopOutlined style={{ fontSize: '3rem' }}  />
+                            )}
                           </Grid>
-                        </Grid>);
-                      })}
-                    </Grid>
-                  </MainCard>
+                          <Grid item xs zeroMinWidth>
+                            <Typography align="left" variant="h5">
+                              {professionalExperience?.title}
+                            </Typography>
+                            <Typography variant="h6">
+                              {professionalExperience?.company}
+                            </Typography>
+                            <Typography color="secondary">
+                              {professionalExperience?.location}
+                            </Typography>
+                            <Typography color="secondary">
+                              {format(new Date(professionalExperience?.startDateUtc), 'MMM y')}&nbsp;-&nbsp;{professionalExperience?.endDateUtc ? format(new Date(professionalExperience?.endDateUtc), 'MMM y') : 'Present'}
+                            </Typography>
+                            <Typography align="left" variant="body1">
+                              {professionalExperience?.description}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>);
+                    }) :
+                    (<Grid item xs={12}>
+                      <Typography>No data.</Typography>
+                    </Grid>)
+                  }
                 </Grid>
-
-              </Grid>
+              </MainCard>
             }
             {tabGroupId === educationsTabGroup &&
-              <Grid container spacing={3}>
-                
-                <Grid item xs={12}>
-                  <MainCard>
+              <MainCard>
 
-                    <Grid container spacing={3}>
-                      <Grid item xs={12}>
-                        <Typography variant="h3">Education</Typography>
-                      </Grid>
-                      {missionContractorMatch?.contractor?.educations?.map((education, educationIndex) => {
-                        return (<Grid key={educationIndex} item xs={12}>
-                          <Grid container spacing={2}>
-                            <Grid item>
-                              {education?.schoolLogoUrl ? 
-                              (
-                                <img
-                                  style={{ width: 50, height: 50, textDecoration: 'none', opacity: 1 }}
-                                  alt={education?.school}
-                                  src={education?.schoolLogoUrl}
-                                />
-                              ) : 
-                              (
-                                <ShopOutlined style={{ fontSize: '3rem' }}  />
-                              )}
-                            </Grid>
-                            <Grid item xs zeroMinWidth>
-                              <Typography align="left" variant="h5">
-                                {education?.degreeName}
-                              </Typography>
-                              <Typography variant="h6">
-                                {education?.school}
-                              </Typography>
-                              <Typography color="secondary">
-                                {education?.fieldOfStudy}
-                              </Typography>
-                              <Typography color="secondary">
-                                {format(new Date(education?.startDateUtc), 'MMM y')}&nbsp;-&nbsp;{education?.endDateUtc ? format(new Date(education?.endDateUtc), 'MMM y') : 'Present'}
-                              </Typography>
-                              <Typography align="left" variant="body1">
-                                {education?.description}
-                              </Typography>
-                            </Grid>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Typography variant="h3">Education</Typography>
+                  </Grid>
+                  {missionContractorMatch?.contractor?.educations?.length > 0 ?
+                    missionContractorMatch?.contractor?.educations?.map((education, educationIndex) => {
+                      return (<Grid key={educationIndex} item xs={12}>
+                        <Grid container spacing={2}>
+                          <Grid item>
+                            {education?.schoolLogoUrl ? 
+                            (
+                              <img
+                                style={{ width: 50, height: 50, textDecoration: 'none', opacity: 1 }}
+                                alt={education?.school}
+                                src={education?.schoolLogoUrl}
+                              />
+                            ) : 
+                            (
+                              <ShopOutlined style={{ fontSize: '3rem' }}  />
+                            )}
                           </Grid>
-                        </Grid>);
-                      })}
-                    </Grid>
-
-                  </MainCard>
+                          <Grid item xs zeroMinWidth>
+                            <Typography align="left" variant="h5">
+                              {education?.degreeName}
+                            </Typography>
+                            <Typography variant="h6">
+                              {education?.school}
+                            </Typography>
+                            <Typography color="secondary">
+                              {education?.fieldOfStudy}
+                            </Typography>
+                            <Typography color="secondary">
+                              {format(new Date(education?.startDateUtc), 'MMM y')}&nbsp;-&nbsp;{education?.endDateUtc ? format(new Date(education?.endDateUtc), 'MMM y') : 'Present'}
+                            </Typography>
+                            <Typography align="left" variant="body1">
+                              {education?.description}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>);
+                    }) :
+                    (<Grid item xs={12}>
+                      <Typography>No data.</Typography>
+                    </Grid>)
+                  }
                 </Grid>
 
-              </Grid>
+              </MainCard>
             }
             {tabGroupId === certificationsTabGroup &&
-              <Grid container spacing={3}>
-                
-                <Grid item xs={12}>
-                  <MainCard>
+              <MainCard>
                     
-                    <Grid container spacing={3}>
-                      <Grid item xs={12}>
-                        <Typography variant="h3">Certification</Typography>
-                      </Grid>
-                      {missionContractorMatch?.contractor?.certifications?.map((certification, certificationIndex) => {
-                        return (<Grid key={certificationIndex} item xs={12}>
-                          <Grid container spacing={2}>
-                            <Grid item>
-                              <FileDoneOutlined style={{ fontSize: '3rem' }}  />
-                            </Grid>
-                            <Grid item xs zeroMinWidth>
-                              <Typography align="left" variant="h5">
-                                {certification?.name}
-                              </Typography>
-                              <Typography variant="h6">
-                                {certification?.authority}
-                              </Typography>
-                              <Typography color="secondary">
-                                {format(new Date(certification?.startDateUtc), 'MMM y')}&nbsp;-&nbsp;{certification?.endDateUtc ? format(new Date(certification?.endDateUtc), 'MMM y') : 'Present'}
-                              </Typography>
-                              
-                              <Typography color="secondary">
-                                {certification?.url}
-                              </Typography>
-                            </Grid>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Typography variant="h3">Certification</Typography>
+                  </Grid>
+                  {missionContractorMatch?.contractor?.certifications?.length > 0 ? 
+                    missionContractorMatch?.contractor?.certifications?.map((certification, certificationIndex) => {
+                      return (<Grid key={certificationIndex} item xs={12}>
+                        <Grid container spacing={2}>
+                          <Grid item>
+                            <FileDoneOutlined style={{ fontSize: '3rem' }}  />
                           </Grid>
-                        </Grid>);
-                      })}
-                    </Grid>
-
-                  </MainCard>
+                          <Grid item xs zeroMinWidth>
+                            <Typography align="left" variant="h5">
+                              {certification?.name}
+                            </Typography>
+                            <Typography variant="h6">
+                              {certification?.authority}
+                            </Typography>
+                            <Typography color="secondary">
+                              {format(new Date(certification?.startDateUtc), 'MMM y')}&nbsp;-&nbsp;{certification?.endDateUtc ? format(new Date(certification?.endDateUtc), 'MMM y') : 'Present'}
+                            </Typography>
+                            
+                            <Typography color="secondary">
+                              {certification?.url}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>);
+                    }) :
+                    (<Grid item xs={12}>
+                      <Typography>No data.</Typography>
+                    </Grid>)
+                  }
                 </Grid>
 
-              </Grid>
+              </MainCard>
             }
           </Grid>
         </Grid>
