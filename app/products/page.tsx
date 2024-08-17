4bb04131d7a9fb05d972e782b368806774e67005
EuvhenRight <ugnivenko.ea@gmail.com>
2024-08-17 23:21:17 +0200

    change layout products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -12,7 +12,7 @@ const Products = async () => {
 			<div className='my-10'>
 				<BreadcrumbProduct />
 			</div>
-			<ul className='grid grid-cols-1 gap-2 md:gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+			<ul className='grid grid-cols-1 gap-2 md:gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
 				{dataProducts.toReversed().map(product => (
 					<li key={product.id}>
 						<ProductsCard product={product} />
