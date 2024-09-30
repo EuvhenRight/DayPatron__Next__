23bbcbf432d71bac57f0ec044eff74d0e8d177d2
EuvhenRight <ugnivenko.ea@gmail.com>
2024-09-30 21:38:39 +0200

    delete admin logic

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,14 +1,4 @@
-import { getCurrentUser } from '@/actions/user'
-
 const AdminDashboard = async () => {
-	const currentUser = await getCurrentUser()
-
-	if (currentUser?.role !== 'ADMIN')
-		return (
-			<p className='flex justify-center items-center font-semibold h-full'>
-				You do not have permission to access ADMIN page
-			</p>
-		)
 	return <div>Dashboard</div>
 }
 
