68c26154366657b326202618476f66051547fff6
EuvhenRight <ugnivenko.ea@gmail.com>
2024-10-01 21:29:21 +0200

    contacts2

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,9 +1,4 @@
 import { getCurrentUser } from '@/actions/user'
-import { ContactsComponent } from '@/components/Contacts/contacts-component'
-import data from '@/lib/db/content.json'
-import { contactData } from '@/lib/services/contacts'
-import { rubikGlitch } from '@/lib/utils/font'
-import { cn } from '@/lib/utils/utils'
 import { Metadata } from 'next'
 
 // METADATA GENERATOR
@@ -37,22 +32,22 @@ export const metadata: Metadata = {
 
 export default async function ContactsPage() {
 	const [currentUser] = await Promise.all([getCurrentUser()])
-	const { ContentContactsPage } = data
+	// const { ContentContactsPage } = data
 
 	return (
 		<div>
 			<section className='container lg:pt-10 relative flex flex-col md:flex-row w-full pt-2 mb-4'>
 				<div className='w-full md:w-1/2 text-start'>
-					<h1 className={cn(rubikGlitch.className, 'text-2xl font-bold')}>
+					{/* <h1 className={cn(rubikGlitch.className, 'text-2xl font-bold')}>
 						{ContentContactsPage.page.title}
-					</h1>
-					<ul className='my-2 pt-4 *:my-4'>
+					</h1> */}
+					{/* <ul className='my-2 pt-4 *:my-4'>
 						{contactData.map(({ icon, text, link }, index) => (
 							<li key={index} className='flex items-center justify-start gap-2'>
 								<ContactsComponent icon={icon} text={text} link={link} />
 							</li>
 						))}
-					</ul>
+					</ul> */}
 				</div>
 				{/* <div className='w-full md:w-1/2 flex flex-col justify-start'>
 					<FeedBackForm currentUser={currentUser} />
