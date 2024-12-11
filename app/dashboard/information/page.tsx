b884e6fea485d1de4a941616182d86cf7cd4f66a
EuvhenRight <ugnivenko.ea@gmail.com>
2024-12-11 22:11:44 +0100

    fix analytics 2

--- app/dashboard/information/page.tsx
+++ app/dashboard/information/page.tsx
@@ -1,6 +1,8 @@
 import { UserInformation } from '@/components/UserInformation/user-information'
 import { getManyOrders, getOrder } from '@/lib/services/order'
 
+// Ensure dynamic rendering
+export const dynamic = 'force-dynamic'
 async function UserInformationPage() {
 	const [order, orders] = await Promise.all([getOrder(), getManyOrders()])
 
