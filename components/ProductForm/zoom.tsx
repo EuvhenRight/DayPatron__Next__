e9d00d921169f5d3f8f88a38d88dc196fcf6e264
EuvhenRight <ugnivenko.ea@gmail.com>
2024-12-26 19:18:12 +0100

    Add arrow logik in zoom7

--- components/ProductForm/zoom.tsx
+++ components/ProductForm/zoom.tsx
@@ -1,6 +1,6 @@
 import { CircleX } from 'lucide-react'
 import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
 
 interface ZoomProps {
@@ -21,49 +21,10 @@ export const Zoom: React.FC<ZoomProps> = ({
 	countImages,
 }) => {
 	const [isZoomedIn, setIsZoomedIn] = useState(false) // State to manage zoom-in or not
-	const containerRef = useRef<HTMLDivElement | null>(null)
-	const touchStartX = useRef<number | null>(null)
-	const touchStartY = useRef<number | null>(null)
-
-	useEffect(() => {
-		// Disable page scrolling when zoom modal is open
-		document.body.style.overflow = 'hidden'
-
-		return () => {
-			// Cleanup when the modal is closed
-			document.body.style.overflow = 'auto'
-		}
-	}, [])
 
 	const handleImageClick = () => {
 		// Toggle zoom in/out on image click
 		setIsZoomedIn(prev => !prev)
-	}
-
-	const handleTouchStart = (e: React.TouchEvent) => {
-		if (isZoomedIn) {
-			touchStartX.current = e.touches[0].clientX
-			touchStartY.current = e.touches[0].clientY
-		}
-	}
-
-	const handleTouchMove = (e: React.TouchEvent) => {
-		if (isZoomedIn && containerRef.current) {
-			const touchEndX = e.touches[0].clientX
-			const touchEndY = e.touches[0].clientY
-
-			if (touchStartX.current !== null && touchStartY.current !== null) {
-				const touchDeltaX = touchStartX.current - touchEndX
-				const touchDeltaY = touchStartY.current - touchEndY
-
-				containerRef.current.scrollLeft += touchDeltaX
-				containerRef.current.scrollTop += touchDeltaY
-
-				// Update the starting positions for the next move
-				touchStartX.current = touchEndX
-				touchStartY.current = touchEndY
-			}
-		}
 	}
 
 	return (
@@ -91,18 +52,16 @@ export const Zoom: React.FC<ZoomProps> = ({
 				<CircleX className='w-8 h-8 text-white' />
 			</button>
 			<div
-				className={`overscroll-contain touch-auto flex items-center justify-center ${
+				className={`touch-auto flex items-center justify-center overflow-auto ${
 					isZoomedIn ? 'max-h-full' : 'max-h-screen'
 				}`}
-				onTouchStart={handleTouchStart}
-				onTouchMove={handleTouchMove}
 			>
 				<Image
 					src={imageUrl}
 					alt='Zoomed image'
 					width={isZoomedIn ? 1200 : 800} // Adjust size on zoom
 					height={isZoomedIn ? 1200 : 800}
-					className={`cursor-zoom-in object-contain transition-transform overscroll-contain duration-300 ${
+					className={`cursor-zoom-in object-fill transition-transform duration-300 ${
 						isZoomedIn ? 'scale-150' : 'scale-100' // Apply scaling on zoom-in
 					}`}
 					onClick={handleImageClick}
